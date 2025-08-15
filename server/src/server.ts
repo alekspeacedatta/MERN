f5f89d03cd3532cfca4737b12be9271bcc047e1a
aleksandre <faleqs33@gmail.com>
2025-08-15 22:08:43 +0400

    improvments3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,31 @@
-import cors from 'cors'
-import dotenv from 'dotenv'
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 import { connectDB } from './config/db';
-import authRoutes from './routes/auth'
-import productRoutes from './routes/product';
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3100
-dotenv.config();
+const port = process.env.PORT || 3100;
+
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
-}))
+}));
 
 app.use(express.json());
+
+import authRoutes from './routes/auth';
+import productRoutes from './routes/product';
 app.use('/api/auth', authRoutes);
 app.use('/api/product', productRoutes);
 
-
 app.get('/api/data', (req, res) => {
-    res.json({ message: 'hello this is message from backend', id: 5 })
+    res.json({ message: 'hello this is message from backend', id: 5 });
 });
 
 connectDB();
-app.listen(3100, () => {
-    console.log('server runs on http://localhost:3100');
-})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
